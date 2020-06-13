3d618386b8393368f3cb83cd7594b41f1aa0e686
JdSierraBeleno <juan.sierra@zabud.com.co>
2020-06-13 13:48:11 -0500

    variables muteables

--- rust.rs
+++ rust.rs
@@ -1,9 +1,17 @@
 fn main() {
-    hola_mundo();
+    variables_mutables();
 }
 
 fn hola_mundo() 
 {
     let a = 5;
+    println!("Hola mundo {}", a);
+}
+
+fn variables_mutables()
+{
+    let mut a = 5;
+    println!("Hola mundo {}", a);
+    a = 20;
     println!("Hola mundo {}", a);
 }
