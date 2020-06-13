911d03d1860559c6bdf966a5bf5eb582b1badeda
JdSierraBeleno <juan.sierra@zabud.com.co>
2020-06-13 15:21:28 -0500

    data types

--- rust.rs
+++ rust.rs
@@ -1,3 +1,9 @@
+fn hola_mundo() 
+{
+    let a = 5;
+    println!("Hola mundo {}", a);
+}
+
 fn variables_mutables()
 {
     let mut a = 5;
@@ -6,13 +12,34 @@ fn variables_mutables()
     println!("Hola mundo {}", a);
 }
 
-fn hola_mundo() 
+/*  Data Type
+    bool
+    char
+    i8      : signed integer 8 bits
+    i16     : signed integer 16 bits
+    i32     : signed integer 32 bits
+    i64     : signed integer 64 bits
+    u8      : unsigned integer 8 bits
+    u16     : unsigned integer 16 bits
+    u32     : unsigned integer 32 bits
+    u64     : unsigned integer 64 bits
+    f32     : point floating 32 bits
+    f64     : point floating 64 bits
+*/
+
+fn data_type(a:i8, b:i8)
 {
-    let a = 5;
-    println!("Hola mundo {}", a);
+    println!("Hola mundo {}", a + b);
+}
+
+fn data_type_dos()
+{
+    let a = 10;
+    let b = &a;
+
+    println!("{}, {}, {:p}, {:p}", a, b, b, &a);
 }
 
 fn main() {
-    variables_mutables();
+    data_type_dos();
 }
-
